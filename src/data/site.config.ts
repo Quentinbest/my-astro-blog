d24d24c06a1c423cab01da077a555cd662abb1b5
tk1026@163.com <tk1026@163.com>
2023-12-19 19:43:26 +0800

    init blog

---
+++ src/data/site.config.ts
@@ -0,0 +1,19 @@
+interface SiteConfig {
+	author: string
+	title: string
+	description: string
+	lang: string
+	ogLocale: string
+	shareMessage: string
+	paginationSize: number
+}
+
+export const siteConfig: SiteConfig = {
+	author: 'Quentin', // Site author
+	title: 'Quentin’s Blog', // Site title.
+	description: 'Share articles about technology.', // Description to display in the meta tags
+	lang: 'en-GB',
+	ogLocale: 'en_GB',
+	shareMessage: 'Share this post', // Message to share a post on social media
+	paginationSize: 6 // Number of posts per page
+}
